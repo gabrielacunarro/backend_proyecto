7e64ebcdc597f9d352be6ea4ea46bb0447483db2
Gabriela Cuñarro <gabrielacunarro@gmail.com>
2024-04-07 18:04:56 -0300

    borrando console.logs

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,7 +5,6 @@ import { connect } from "mongoose"
 
 export default async function dbConnection() {
     try {
-        console.log("Valor de DB_LINK:", process.env.DB_LINK);
         await connect(process.env.DB_LINK)
         console.log("database connected")
     } catch (error) {
